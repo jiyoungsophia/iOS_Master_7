d8d42a375d4a36eeeed3c5dcd4050dd59ba89915
jiyoungsophia <sophiathedev@gmail.com>
2025-06-04 17:19:44 +0900

    Add: 프로그래밍 심화 주차 과제 세팅

---
+++ 03SwiftAdvancedPractice/03SwiftAdvancedPractice/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  03SwiftAdvancedPractice
+//
+//  Created by 지영 on 6/4/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+// MARK: - 필수 문제 1
+
+
+
+// MARK: - 필수 문제 2
+
+
+
+// MARK: - 필수 문제 3
