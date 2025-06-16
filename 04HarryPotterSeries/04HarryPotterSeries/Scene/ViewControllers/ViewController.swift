01b463042224ed791e7a4bc21b9e5aebb838900c
jiyoungsophia <sophiathedev@gmail.com>
2025-06-16 13:55:53 +0900

    Add: 책시리즈 과제 초기 세팅
    - SnapKit, Then 패키지 추가
    - SwiftLint 세팅
    - 필요한 Asset 추가
    - 폴더 정리

---
+++ 04HarryPotterSeries/04HarryPotterSeries/Scene/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  04HarryPotterSeries
+//
+//  Created by Milou on 6/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBlue
+    }
+}
