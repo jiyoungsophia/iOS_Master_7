04ec7fdbdc1a6645d0ce93f452cfbf1d02c96728
jiyoungsophia <sophiathedev@gmail.com>
2025-06-18 21:04:22 +0900

    Refactor: #50- 과제 피드백 반영
    - 불필요한 변수 할당 제거 및 함수형 스타일 개선

--- 03SwiftAdvancedPractice/03SwiftAdvancedPractice/HigherOrderFunctions.swift
+++ 03SwiftAdvancedPractice/03SwiftAdvancedPractice/HigherOrderFunctions.swift
@@ -10,24 +10,21 @@ import Foundation
 /// check 1
 func mapIntToString() -> [String] {
     let numbers = Array(1...5)
-    var result = [String]()
-
-    result = numbers.map { String($0) }
-    return result
+    return numbers
+        .map { String($0) }
 }
 
 /// check 2
 func filterEvenNumbersToString() -> [String] {
     let input = Array(1...10)
-    var output : Array<String>
-
-    output = input.filter { $0 % 2 == 0 }.compactMap { String($0) }
-    return output
+    return input
+        .filter { $0 % 2 == 0 }
+        .compactMap { String($0) }
 }
 
 /// check 3
 func myMap(_ array: [Int], _ transform: (Int) -> String) -> [String] {
-    var output : [String] = []
+    var output: [String] = []
     
     for element in array {
         output.append(transform(element))
