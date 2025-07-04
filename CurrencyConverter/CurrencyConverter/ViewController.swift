a964d99049f8b6203e203b3ae371f98eb1b08de5
jiyoungsophia <sophiathedev@gmail.com>
2025-07-04 21:02:14 +0900

    Chore: 환율 계산기 프로젝트 세팅

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Milou on 7/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemOrange
+    }
+
+
+}
+
